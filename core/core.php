2e4cfe5ed595cb36618b0aa80ff90a772779cb2c
gentlespoon <songangda@gmail.com>
2017-11-08 12:00:36 -0800

    UPDATE: include API mode in router

--- core/core.php
+++ core/core.php
@@ -13,25 +13,29 @@ if (!defined("ROOT")) define("ROOT", $_SERVER['DOCUMENT_ROOT']."/");
 if (!isset($GLOBALS['startTime'])) $GLOBALS['startTime'] = microtime(true);
 
 // load DB/Locale/Datetime configuration
-if (file_exists(ROOT."config/config.php")) require_once(ROOT."config/config.php"); else die("FATAL ERROR: Configuration file <span style='font-family: monospace'>config/config.php</span> does not exist.");
+if (file_exists(ROOT."config/config.php")) {
+  require_once(ROOT."config/config.php");
+} else {
+  exit("FATAL ERROR: Configuration file <span style='font-family: monospace'>config/config.php</span> does not exist.");
+}
 // load language file, not necessary for this project, but always a good practice to separate code and translations
+// import functions
+require_once(ROOT."core/function.inc.php");
+foreach (scandir(ROOT.'core/class') as $filename) {
+  $path = ROOT.'core/class/'.$filename;
+  if (is_file($path)) require_once $path;
+}
 require_once(ROOT."locales/".$config['locale'].".php");
 
+
 // using meekrodb in procedural way
-require_once(ROOT."core/lib/meekrodb.2.3.class.php");
+// require_once(ROOT."core/lib/meekrodb.2.3.class.php");
 DB::$host = $config['db']['host'];
 DB::$port = $config['db']['port'];
 DB::$user = $config['db']['username'];
 DB::$password = $config['db']['password'];
 DB::$dbName = $config['db']['dbname'];
 DB::$encoding = $config['db']['charset'];
-
-// import functions
-require_once(ROOT."core/function.inc.php");
-foreach (scandir(ROOT.'core/class') as $filename) {
-    $path = ROOT.'core/class/'.$filename;
-    if (is_file($path)) require_once $path;
-}
 
 // initialize output
 if (!isset($GLOBALS['output'])) $GLOBALS['output'] = [ "alert" => [], "title" => ""];
--- index.php
+++ index.php
@@ -7,7 +7,6 @@ require(ROOT."core/core.php");
 
 // path => filename (/module/*.inc.php)
 $route = [
-  "jobs",
   "member",
   "questions",
   "tags",
@@ -21,10 +20,20 @@ foreach($route as $v) $GLOBALS['output']['activeNav'][$v] = "";
 // $2 = action
 // $3... = real $_GET
 
+
 if (array_key_exists("path", $_GET)) $path = explode("/", $_GET['path']);
 else $path = ["questions"]; // default page
 
-if ($path[0] == "api") require_once("api/".$path[1]."/".$path[2].".php");
+if ($path[0] == "api") {
+  unset($path[0]);
+  $file = ROOT."api/".implode("/", $path).".php";
+  if (is_file($file)) {
+    require_once($file);
+  }
+  else {
+    header("HTTP/1.0 400 Bad Request");
+  }
+}
 else {
   if (in_array($path[0], $route)) {
     $action = "";
