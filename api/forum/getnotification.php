9eaf7202e4698e53086accc00a987471314cbb9f
xzhou016 <xzhou016@ucr.edu>
2017-11-15 23:14:02 -0800

    UPDATE now get thread id and title

---
+++ api/forum/getnotification.php
@@ -0,0 +1,17 @@
+<?php
+
+
+ $notSeen = DB::query("SELECT forum_update.tid, forum_threads.title FROM forum_update LEFT JOIN forum_threads ON forum_threads.tid=forum_update.tid WHERE forum_update.uid=%i", $GLOBALS['curUser']['uid']);
+//printv($notSeen);
+ $tidArray = [];
+ foreach ($notSeen as $key => $value) {
+ 	$temp = [$value['tid'], $value['title']];
+ 	if (!in_array($temp, $tidArray)) {
+ 		array_push($tidArray, $temp);
+ 	}
+ }
+
+DB::query("DELETE FROM forum_update WHERE uid = %i", $GLOBALS['curUser']['uid']);
+
+api_write(1, $tidArray);
+// printv($tidArray);
