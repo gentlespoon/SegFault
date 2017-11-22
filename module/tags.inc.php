e9fcf5b1125f7e45f7f84aa4cf76554147116f5e
Angda Song <songangda@gmail.com>
2017-11-22 13:00:57 -0800

    NEW: isFav tag indicate bit

--- module/tags.inc.php
+++ module/tags.inc.php
@@ -6,15 +6,18 @@ $tags = tags::getTags();
 $favTags = tags::getFavTags($_SESSION['uid']);
 
 $sortedTags = [];
+
+foreach($tags as $tagid => $taginfo) {
+  $sortedTags[$tagid] = $taginfo;
+  $sortedTags[$tagid]['isFav'] = 0;
+}
+
 foreach($favTags as $tagid => $taginfo) {
   $sortedTags[$tagid] = $taginfo;
   unset($tags[$tagid]);
-}
-foreach($tags as $tagid => $taginfo) {
-  $sortedTags[$tagid] = $taginfo;
+  $sortedTags[$tagid]['isFav'] = 1;
 }
 
 $GLOBALS['output']['tags'] = $sortedTags;
-
 
 template("tags");
